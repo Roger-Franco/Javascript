68bf4e270dd84ac48320f462c3100e32acb9ad5f
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-06 13:24:24 -0400

    Inicio e busca de elementos através de seletores CSS

---
+++ client/app/index.js
@@ -0,0 +1,9 @@
+
+var campos = [
+  document.querySelector('#data'),
+  document.querySelector('#valor'),
+  document.querySelector('#quantidade')
+];
+
+
+console.log(campos); // verificando o conteúdo do array
