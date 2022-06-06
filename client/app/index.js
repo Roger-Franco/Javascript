1089dd04bb20397ecc7ee8bcba4e6333d0b22d19
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-06 16:09:30 -0400

    Testando a chamada na submissao do form

--- client/app/index.js
+++ client/app/index.js
@@ -7,3 +7,10 @@ var campos = [
 
 
 console.log(campos); // verificando o conteúdo do array
+
+// precisamos de tbody, pois ele receberá a tr que vamos construir
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  alert('Oi!!')
+})
