680a7c30010380634f455531a0b90dc3dd84ff38
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-08 10:11:26 -0400

    A sintaxe get

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,24 @@
 
 class Negociacao {
-  constructor() {
-    this.data = new Date(); // data atual
-    this.quantidade = 1;
-    this.valor = 0.0;
+  constructor(data, quantidade, valor) {
+    this._data = data; // data atual
+    this._quantidade = quantidade;
+    this._valor = valor;
 
+  }
+
+  get volume() {
+    return this._quantidade * this._valor;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  get quantidade() {
+    return this._quantidade;
+  }
+  get valor() {
+    return this._valor;
   }
 }
