42ce15e237e70b20d18831fcc0704c0f6b1d5827
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-11 17:27:23 -0400

    A boa pratica do fail-fast: envio de erro com a ajuda de expressões regulares se a data estiver escrita errada

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -10,6 +10,9 @@ class DateConverter {
     return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
   }
   static paraData(texto) {
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
+      throw new Error('Deve estar no formato aaaa-mm-dd');
+    }
     return new Date(...texto.split('-')
       .map((item, indice) => item - indice % 2))
   }
