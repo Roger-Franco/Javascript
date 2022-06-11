f11b1b0cf5fe4460bd7087bfe8207814b4696404
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-11 17:18:03 -0400

    4.3 TEMPLATE LITERAL

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -7,9 +7,7 @@ class DateConverter {
     throw new Error('Esta classe não pode ser instanciada');
   }
   static paraTexto(data) {
-    return data.getDate()
-      + '/' + (data.getMonth() + 1)
-      + '/' + data.getFullYear();
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
   }
   static paraData(texto) {
     return new Date(...texto.split('-')
