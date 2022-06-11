4f94c6081fd49f5fbc6c74d76ee000071f875bb4
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-11 13:07:24 -0400

    Isolando as responsabilidades de conversão de datas

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,17 +10,17 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(...this._inputData.value.split('-')
-      .map((item, indice) => item - indice % 2)
-    );
+    let converter = new DateConverter();
+
+    let data = converter.paraData(this._inputData.value);
+
     let negociacao = new Negociacao(
       data,
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value),
     )
-    let diaMesAno = negociacao.data.getDate()
-      + '/' + (negociacao.data.getMonth() + 1)
-      + '/' + negociacao.data.getFullYear();
+    let diaMesAno = converter.paraTexto(negociacao.data);
     console.log(diaMesAno);
+    console.log(data);
   }
 }
