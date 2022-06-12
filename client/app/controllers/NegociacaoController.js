c923c8da4ff96533a3f42d80aad3422f16ff3575
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-12 12:51:36 -0400

    Encapsulando

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,22 +1,31 @@
 
 class NegociacaoController {
   constructor() {
-    let $ = document.querySelector.bind(document); // precisei adicionar o .bind(document);
-
+    let $ = document.querySelector.bind(document);
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._negociacoes = new Negociacoes();
   }
   adiciona(event) {
     event.preventDefault();
 
-    // CHAMANDO O MÉTODO ESTÁTICO
-    let negociacao = new Negociacao(
+    this._negociacoes.adiciona(this._criaNegociacao())
+    console.log(this._negociacoes.paraArray());
+    this._limpaFormulario();
+  }
+  _limpaFormulario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0
+    this._inputData.focus();
+  }
+  _criaNegociacao() {
+    // retorna uma instância de negociação
+    return new Negociacao(
       DateConverter.paraData(this._inputData.value),
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value),
-    )
-    let diaMesAno = DateConverter.paraTexto(negociacao.data);
-    console.log(diaMesAno);
+    );
   }
 }
