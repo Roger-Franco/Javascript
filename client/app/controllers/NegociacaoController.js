47e9e8bf02d6e0950972ca7211f42ee8d016de24
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-13 21:45:12 -0400

    renderizando a tabela com a View

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,6 +6,10 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
     this._negociacoes = new Negociacoes();
+    // passamos para o construtor o seletor CSS de ID
+    this._negociacoesView = new NegociacoesView('#negociacoes');
+    // atualizando a view
+    this._negociacoesView.update();
   }
   adiciona(event) {
     event.preventDefault();
