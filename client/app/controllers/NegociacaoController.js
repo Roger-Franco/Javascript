da5d895ee8b0fc62bbbdb512b3b3d3b48d88c7c2
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-16 12:38:26 -0400

    Cirando a classe mensagemview

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -12,21 +12,28 @@ class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     // instanciando o modelo!
     this._mensagem = new Mensagem();
-
+    // nova propriedade!
+    this._mensagemView = new MensagemView('#mensagemView');
+    this._mensagemView.update(this._mensagem);
   }
+
   adiciona(event) {
     event.preventDefault();
     this._negociacoes.adiciona(this._criaNegociacao())
     this._mensagem.texto = 'Negociação adicionada com sucesso';
     this._negociacoesView.update(this._negociacoes);
+    // atualiza a view com o texto da mensagem que acabamos de atribuir
+    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
   }
+
   _limpaFormulario() {
     this._inputData.value = '';
     this._inputQuantidade.value = 1;
     this._inputValor.value = 0.0
     this._inputData.focus();
   }
+
   _criaNegociacao() {
     // retorna uma instância de negociação
     return new Negociacao(
