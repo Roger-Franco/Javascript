9077fc6d3c308ebacd843d0aa667addd29439a40
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-16 21:35:57 -0400

    Criando a funcionalidade apagar

--- client/app/app.js
+++ client/app/app.js
@@ -5,9 +5,8 @@ const controller = new NegociacaoController();
 
 document
   .querySelector('.form')
-  // .addEventListener('submit', function (event) {
-  //   controller.adiciona(event);
-  // });
-  //  Ou pode ser como abaixo. Menos verboso
-  // bind aqui!
   .addEventListener('submit', controller.adiciona.bind(controller));
+
+document
+  .querySelector('#botao-apaga')
+  .addEventListener('click', controller.apaga.bind(controller));
--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -2,16 +2,27 @@
 class Negociacoes {
   constructor() {
     this._negociacoes = [];
+    // CONGELOU A INSTÂNCIA
+    Object.freeze(this);
+
   }
+
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
   }
+
   paraArray() {
     return [].concat(this._negociacoes);
   }
+
   get volumeTotal() {
     return this._negociacoes
       .reduce((total, negociacao) =>
         total + negociacao.volume, 0);
   }
+
+  esvazia() {
+    this._negociacoes.length = 0;
+  }
+
 }
