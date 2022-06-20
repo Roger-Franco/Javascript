df2b3ede0ddf77a8c853ee509afa1c3d1ce30eb9
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-19 22:40:26 -0400

    criando mais um service para isolar xmlhttprequest

--- client/app/domain/negociacao/NegociacaoService.js
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -1,38 +1,24 @@
 class NegociacaoService {
 
+  constructor() {
+    // NOVA PROPRIEDADE!
+    this._http = new HttpService();
+  }
+
   obterNegociacoesDaSemana() {
-    return new Promise((resolve, reject) => {
+    return this._http
+      .get('negociacoes/semana')
+      .then(
+        dados => {
+          const negociacoes = dados.map(objeto =>
+            new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
+          return negociacoes;
+        },
+        err => {
+          // ATENÇÃO AQUI!
+          throw new Error('Não foi possível obter as negociações');
 
-      // 0: requisição ainda não iniciada;
-      // 1: conexão com o servidor estabelecida;
-      // 2: requisição recebida;
-      //  3: processando requisição;
-      //  4: requisição está concluída e a resposta está pronta.
-      // OBS: Por esse motivo tem que ser igual a 4
-
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'negociacoes/semana');
-
-      xhr.onreadystatechange = () => {
-
-        if (xhr.readyState == 4) {
-
-          if (xhr.status == 200) {
-
-            const negociacoes = JSON
-              .parse(xhr.responseText)
-              .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
-            // CHAMOU RESOLVE
-            resolve(negociacoes);
-
-          } else {
-            // CHAMOU REJECT
-            reject('Não foi possível obter nas negociações da semana');
-          }
         }
-      };
-
-      xhr.send();
-    })
+      )
   }
 }
