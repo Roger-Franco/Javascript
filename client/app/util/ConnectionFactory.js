dfb46303a69d111b543a350c2bd91ae35c01f09c
Roger-Franco <rogerfrancogalt@gmail.com>
2022-06-22 11:55:47 -0400

    Mudanças para aplicar o padrão de projeto Module Pattern

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,41 +1,48 @@
-const stores = ['negociacoes'];
+const ConnectionFactory = (function () {
 
-let connection = null;
+  const stores = ['negociacoes'];
 
-class ConnectionFactory {
-  constructor() {
-    throw new Error('Não é possível criar instâncias dessa classe');
+  let connection = null;
+  // RETORNA A DEFINIÇÃO DA CLASSE
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error('Não é possível criar instâncias dessa classe');
+    }
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        // SE UMA CONEXÃO JÁ FOI CRIADA,
+        // JÁ PASSA PARA RESOLVE E RETORNA LOGO!
+        if (connection) return resolve(connection);
+        const openRequest = indexedDB.open('jscangaceiro', 2);
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result);
+
+        };
+        openRequest.onsuccess = e => {
+          // SÓ SERÁ EXECUTADO NA PRIMEIRA VEZ QUE A CONEXÃO FOR CRIADA
+          connection = e.target.result;
+          resolve(e.target.result);
+        };
+        openRequest.onerror = e => {
+          console.log(e.target.error)
+          // passa o erro para reject da promise!
+          reject(e.target.error.name)
+        };
+      });
+    }
+
+    static _createStores(connection) {
+      stores.forEach(store => {
+        // if sem bloco, mais sucinto!
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
   }
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      // SE UMA CONEXÃO JÁ FOI CRIADA,
-      // JÁ PASSA PARA RESOLVE E RETORNA LOGO!
-      if (connection) return resolve(connection);
-      const openRequest = indexedDB.open('jscangaceiro', 2);
-      openRequest.onupgradeneeded = e => {
-        ConnectionFactory._createStores(e.target.result);
-
-      };
-      openRequest.onsuccess = e => {
-        // SÓ SERÁ EXECUTADO NA PRIMEIRA VEZ QUE A CONEXÃO FOR CRIADA
-        connection = e.target.result;
-        resolve(e.target.result);
-      };
-      openRequest.onerror = e => {
-        console.log(e.target.error)
-        // passa o erro para reject da promise!
-        reject(e.target.error.name)
-      };
-    });
-  }
-
-  static _createStores(connection) {
-    stores.forEach(store => {
-      // if sem bloco, mais sucinto!
-      if (connection.objectStoreNames.contains(store))
-        connection.deleteObjectStore(store);
-
-      connection.createObjectStore(store, { autoIncrement: true });
-    });
-  }
-}
+})()
+// tmp(); // chama a função!
+// A VARIÁVEL VIVE NO ESCOPO GLOBAL
+// PORQUE FOI DECLARADA FORA DA FUNÇÃO
+// const ConnectionFactory = tmp();
